d0115c309ddbf6ce648ec25d0f6f7e7ff117682d
Agneya Pathare <61740206+agneya-1402@users.noreply.github.com>
2024-10-26 21:39:17 +0530

    Update Servo_1.ino

--- Servo_1/Servo_1.ino
+++ Servo_1/Servo_1.ino
@@ -1,26 +1,25 @@
-#include <Servo.h> // Include the Servo library
+#include <Servo.h> 
 
-Servo servo_1;     // Create a Servo object
+Servo servo_1;     
 
 void setup() {
-  Serial.begin(9600);   // Start serial communication at 9600 baud
-  servo_1.attach(9);    // servo to pin 9
+  Serial.begin(9600);  
+  servo_1.attach(9);    
   Serial.println("Enter angle (0-180):");  // Print in Serial Monitor
 }
 
 void loop() {
-  if (Serial.available() > 0) {            // Check if data is available to read
-    int angle = Serial.parseInt();         // Read the input as an integer
+  if (Serial.available() > 0) {           
+    int angle = Serial.parseInt();         
 
     // range
     if (angle >= 0 && angle <= 180) {
-      servo_1.write(angle);                // Set the servo position
+      servo_1.write(angle);                // Set servo position
       Serial.print("Servo moved to: ");
-      Serial.println(angle);               // Print the angle
+      Serial.println(angle);              
     } else {
       Serial.println("Invalid angle!");
     }
-    
-    delay(10);  // Small delay for stability
+    delay(10); 
   }
 }
